e123e3cb5f61d20d3b4f3bb10a5cb8080a74cf9c
Osvando Neto <42206168+osvaldosneto@users.noreply.github.com>
2020-08-20 21:28:58 -0300

    Add files via upload

---
+++ PSD/Codigos/AE4/parametros.m
@@ -0,0 +1,8 @@
+%parametros
+fa = 8000 %frequencia de amostragem
+Ta = 1/fa %tempo de amostragem
+spf = 1 %amostras por frame
+v1 = 0.2 %amplitude seno
+f1 = 500 %frequencia seno
+G = 0.05 %ganho
+fonte=1
